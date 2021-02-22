d051d4e1c9f47e9247a8630e093f0610a276c038
Matt <mattpodge@gmail.com>
2021-02-22 10:14:38 +0000

    Initial setup of project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+
+            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(Input.touches[i].position);
+
+            if (i == 0)
+            {
+                transform.LookAt(touchPosition, Vector3.forward);
+            }
+        }
+    }
+}
