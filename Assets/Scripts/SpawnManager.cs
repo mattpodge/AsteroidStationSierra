66ca498ac98d232e2a414c4dbd74c4945e06f165
Matt <mattpodge@gmail.com>
2021-03-01 18:10:41 +0000

    Refactoring the asteroid spawn manager

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,102 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
 public class SpawnManager : MonoBehaviour
 {
+    public List<GameObject> lgAsteroids;
+    public List<GameObject> spawnPoints;
 
-    public GameManager gm;
+    public int currentWave = 1;
 
-    [SerializeField] private List<GameObject> spawnPoints;
-    [SerializeField] private List<GameObject> asteroidsPrefab;
+    private int asteroidActiveCount;
+    public int maxAsteroidCount = 5;
 
-    private Camera cam;
-    private float camHeight;
-    private float camWidth;
+    private float xMin = -10;
+    private float xMax = 10;
 
-    private float rangeX;
-    private float rangeY;
+    private float yMin = -10;
+    private float yMax = 10;
 
-    public int asteroidCount;
-    public int asteroidsActive;
-    public int nextWave;
-    private int minWaveTarget;
-    private int maxWaveTarget;
-    public int maxAsteroids;
-
-    private float minDelay;
-    private float maxDelay;
-
-    // Start is called before the first frame update
     void Start()
     {
-        gm = GameObject.Find("GameManager").GetComponent<GameManager>();
-
-        cam = Camera.main;
-        camHeight = cam.orthographicSize;
-        camWidth = camHeight * cam.aspect;
-
-        rangeX = camWidth / 2;
-        rangeY = camHeight / 2;
-
-        asteroidCount = 0;
-        nextWave = gm.currentWave + 1;
-        minWaveTarget = 0;
-        maxWaveTarget = nextWave * 5;
-        maxAsteroids = 5;
-
-        minDelay = 1.0f;
-        maxDelay = 3.0f;
-
         StartCoroutine(SpawnAsteroids());
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
+        asteroidActiveCount = GameObject.FindGameObjectsWithTag("Asteroid").Length;
 
-        asteroidsActive = GameObject.FindGameObjectsWithTag("Asteroid").Length;
-
-        int upgradeNum = minWaveTarget + maxWaveTarget;
-
-        if (gm.asteroidsDestroyed == upgradeNum)
-        {
-            gm.currentWave += 1;
-            nextWave += 1;
-            minWaveTarget = upgradeNum;
-            maxWaveTarget = nextWave * 5;
-            maxAsteroids += 5;
-
-            minDelay -= (gm.currentWave * 0.1f);
-            maxDelay -= (gm.currentWave * 0.2f);
-
-            gm.UpdateWave(gm.currentWave);
-        }
+        Debug.Log("Active Asteroids: " + asteroidActiveCount + ", Max Asteroids: " + maxAsteroidCount);
     }
-
 
     IEnumerator SpawnAsteroids()
     {
-        
-        while (asteroidsActive < maxAsteroids)
+        Vector2 spawnPos;
+
+        while(true)
         {
-            Vector3 spawnPosition;
-            int i = Random.Range(0, spawnPoints.Count);
-            int asteroid = Random.Range(0, asteroidsPrefab.Count);
+            int spawnIndex = Random.Range(0, spawnPoints.Count);
+            float randomXPos = Random.Range(xMin, xMax);
+            float randomYPos = Random.Range(yMin, yMax);
+            int asteroidIndex = Random.Range(0, lgAsteroids.Count);
 
-            if (spawnPoints[i].CompareTag("SpawnManY"))
+            spawnPos = (spawnPoints[spawnIndex].CompareTag("SpawnPtVert") ? new Vector3(spawnPoints[spawnIndex].transform.position.x, randomYPos, 0f) : new Vector3(randomXPos, spawnPoints[spawnIndex].transform.position.y, 0f));
+            
+            if (asteroidActiveCount < maxAsteroidCount)
             {
-                spawnPosition = new Vector3(spawnPoints[i].transform.position.x, Random.Range(-rangeY, rangeY), 0.0f);
-                Instantiate(asteroidsPrefab[asteroid], spawnPosition, transform.rotation);
-            }
-            else if (spawnPoints[i].CompareTag("SpawnManX"))
-            {
-                spawnPosition = new Vector3(Random.Range(-rangeX, rangeX), spawnPoints[i].transform.position.y, 0.0f);
-                Instantiate(asteroidsPrefab[asteroid], spawnPosition, transform.rotation);
+                Instantiate(lgAsteroids[asteroidIndex], spawnPos, lgAsteroids[asteroidIndex].transform.rotation);
             }
 
-            asteroidCount += 1;
-            yield return new WaitForSeconds(Random.Range(minDelay, maxDelay));
+            yield return new WaitForSeconds(1.0f);
         }
+
     }
 }
