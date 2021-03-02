5261accc7381045df0815fc822bd0e2d9dfe626a
Matt <mattpodge@gmail.com>
2021-03-02 12:18:29 +0000

    Dynamic spawnpoints and sensors to better fit multiple screen sizes

---
+++ Assets/Scripts/CameraSize.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class CameraSize : MonoBehaviour
+{
+    public static new Camera camera = Camera.main;
+    public static float height = camera.orthographicSize;
+    public static float width = height * camera.aspect;
+}
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,52 +4,121 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public int currentWave, asteroidCountIncrement;
+    public float minSpawnDelay, maxSpawnDelay;
     public List<GameObject> lgAsteroids;
-    public List<GameObject> spawnPoints;
 
-    public int currentWave = 1;
+    private int asteroidActiveCount, maxAsteroidCount;
+    private float xSpawnRange, ySpawnRange;
+    private GameObject[] sensors, spawnPoints;
 
-    private int asteroidActiveCount;
-    public int maxAsteroidCount = 5;
-
-    private float xMin = -10;
-    private float xMax = 10;
-
-    private float yMin = -10;
-    private float yMax = 10;
 
     void Start()
     {
+        // Set our spawn ranges based on camera/screen size
+        xSpawnRange = CameraSize.width * 1.25f;
+        ySpawnRange = CameraSize.height * 1.25f;
+
+        SpawnPointTransforms();
+        SensorTransforms();
         StartCoroutine(SpawnAsteroids());
     }
 
     private void Update()
     {
+        // Count how many asteroids are currently active
         asteroidActiveCount = GameObject.FindGameObjectsWithTag("Asteroid").Length;
 
-        Debug.Log("Active Asteroids: " + asteroidActiveCount + ", Max Asteroids: " + maxAsteroidCount);
+        // Increase the max available asteroids with wave increase
+        maxAsteroidCount = currentWave * asteroidCountIncrement;
+
+        Debug.Log(minSpawnDelay);
+
+        // Debug.Log("Active Asteroids: " + asteroidActiveCount + ", Max Asteroids: " + maxAsteroidCount);
     }
 
     IEnumerator SpawnAsteroids()
     {
-        Vector2 spawnPos;
-
         while(true)
         {
-            int spawnIndex = Random.Range(0, spawnPoints.Count);
-            float randomXPos = Random.Range(xMin, xMax);
-            float randomYPos = Random.Range(yMin, yMax);
+            float spawnDelay = Random.Range(minSpawnDelay, maxSpawnDelay);
+            float randomXPos = Random.Range(-xSpawnRange, xSpawnRange);
+            float randomYPos = Random.Range(-ySpawnRange, ySpawnRange);
+            int spawnIndex = Random.Range(0, spawnPoints.Length);
             int asteroidIndex = Random.Range(0, lgAsteroids.Count);
 
-            spawnPos = (spawnPoints[spawnIndex].CompareTag("SpawnPtVert") ? new Vector3(spawnPoints[spawnIndex].transform.position.x, randomYPos, 0f) : new Vector3(randomXPos, spawnPoints[spawnIndex].transform.position.y, 0f));
+            // Spawn position is dependant on which spawn point is instantiating the asteroid
+            Vector2 spawnPos = (
+                spawnPoints[spawnIndex].transform.position.x > 0f ? 
+                new Vector2(spawnPoints[spawnIndex].transform.position.x, randomYPos) : 
+                new Vector2(randomXPos, spawnPoints[spawnIndex].transform.position.y)
+            );
             
+            // Some randomness to when asteroids spawn
+            yield return new WaitForSeconds(spawnDelay);
+
+            // As long as we haven't hit our max asteroid count, spawn an asteroid
             if (asteroidActiveCount < maxAsteroidCount)
             {
                 Instantiate(lgAsteroids[asteroidIndex], spawnPos, lgAsteroids[asteroidIndex].transform.rotation);
             }
-
-            yield return new WaitForSeconds(1.0f);
         }
+    }
 
+    void SpawnPointTransforms()
+    {
+        // Find our spawnpoints
+        spawnPoints = GameObject.FindGameObjectsWithTag("SpawnPoint");
+
+        // Position spawn points relative to camera/screem size
+        foreach (GameObject spawnPoint in spawnPoints)
+        {
+            switch (spawnPoint.name)
+            {
+                case "SpawnPointTop":
+                    spawnPoint.transform.position = new Vector2(0f, CameraSize.height + 1f);
+                    break;
+                case "SpawnPointRight":
+                    spawnPoint.transform.position = new Vector2(CameraSize.width + 1f, 0f);
+                    break;
+                case "SpawnPointLeft":
+                    spawnPoint.transform.position = new Vector2(-(CameraSize.width + 1f), 0f);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    void SensorTransforms()
+    {
+        // Find our sensors
+        sensors = GameObject.FindGameObjectsWithTag("Sensor");
+
+        // Position & resize sensors relative to camera/screen size
+        foreach (GameObject sensor in sensors)
+        {
+            switch (sensor.name)
+            {
+                case "SensorTop":
+                    sensor.transform.position = new Vector2(0f, CameraSize.height + 2.5f);
+                    sensor.transform.localScale = new Vector2(CameraSize.width * 4, 1f);
+                    break;
+                case "SensorRight":
+                    sensor.transform.position = new Vector2(CameraSize.width + 2.5f, 0f);
+                    sensor.transform.localScale = new Vector2(1f, CameraSize.height * 4);
+                    break;
+                case "SensorBottom":
+                    sensor.transform.position = new Vector2(0f, -(CameraSize.height + 2.5f));
+                    sensor.transform.localScale = new Vector2(CameraSize.width * 4, 1f);
+                    break;
+                case "SensorLeft":
+                    sensor.transform.position = new Vector2(-(CameraSize.width + 2.5f), 0f);
+                    sensor.transform.localScale = new Vector2(1f, CameraSize.height * 4);
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
